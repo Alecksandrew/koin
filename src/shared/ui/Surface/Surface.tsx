9ce5b77a0e15c0941492b87e7b4d986c8a3c483f
Alecksandrew <alecksandrewcontato@gmail.com>
2026-05-01 23:03:48 -0300

    fix(Surface): fix component types

--- src/shared/ui/Surface/Surface.tsx
+++ src/shared/ui/Surface/Surface.tsx
@@ -1,17 +1,17 @@
-import { ElementType, HTMLAttributes, ReactNode } from "react";
+import {  HTMLAttributes, HTMLElementType, ReactNode } from "react";
 import { SurfaceVariant, variants } from "./variants";
 
 export interface SurfaceProps extends Omit<HTMLAttributes<HTMLElement>, "className">, SurfaceVariant {
-  as: ElementType;
+  as: HTMLElementType;
   children?: ReactNode;
 }
 
-export function Surface({ as: HTMLTag, children, variant, boxShadow, ...props }: SurfaceProps) {
+export function Surface({ as: HTMLTag, children, variant, boxShadow, hidden, ...props }: SurfaceProps) {
   const isEmpty =
     children === null || children === undefined || children === "";
 
   return (
-    <HTMLTag className={variants({ variant, boxShadow })} {...props} hidden={isEmpty}>
+    <HTMLTag className={variants({ variant, boxShadow })} {...props} hidden={hidden ?? isEmpty}>
       {children}
     </HTMLTag>
   );
